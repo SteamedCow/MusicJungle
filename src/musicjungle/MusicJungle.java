a24d7fa963943f7c7b9dfe80320f0b11f00b82e8
SteamedCow <lasse1991@yahoo.dk>
2015-12-17 02:18:25 +0100

    Added: Game GUI

--- src/musicjungle/MusicJungle.java
+++ src/musicjungle/MusicJungle.java
@@ -1,13 +1,25 @@
 package musicjungle;
 
+import javax.swing.JFrame;
+import musicjungle.gui.game.GameController;
+
 /**
  * MusicJungle
- * @author Lasse
+ * @author SteamedCow
  * @version 17-12-2015
  */
 public class MusicJungle 
 {
     public static void main(String[] args) {
+        JFrame frame = new JFrame("TEMP");
+        GameController controller = new GameController(4, 10);
         
+        frame.setSize(500, 500);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(controller.getPanel());
+        frame.setVisible(true);
+        
+        GameController.startRound();
     }
 }
--- src/musicjungle/gui/game/GameModel.java
+++ src/musicjungle/gui/game/GameModel.java
@@ -1,11 +1,97 @@
 package musicjungle.gui.game;
+import javax.swing.JButton;
+import javax.swing.JProgressBar;
 
 /**
  * GameModel
- * @author Lasse
+ * @author SteamedCow
  * @version 17-12-2015
  */
-public class GameModel 
+public final class GameModel implements Runnable
 {
+    private final GameView view;
+    private final int roundTime;
+    private final JButton[] buttons;
+    private final JProgressBar timer;
     
+    private int round = 0;
+    private long t0, t;
+    private boolean running = true;
+
+    protected GameModel(GameView view, int buttonCount, int roundTime) {
+        this.view = view;
+        
+        final int bc;
+        if(buttonCount > 9)
+            bc = 9;
+        else if(buttonCount < 2)
+            bc = 2;
+        else
+            bc = buttonCount;
+        buttons = new JButton[bc];
+        setButtons(bc);
+        
+        if(roundTime > 10000)
+            this.roundTime = 10000;
+        else if(roundTime < 1000) {
+            if(roundTime == -1)
+                this.roundTime = -1;
+            else
+                this.roundTime = 1000;
+        }
+        else
+            this.roundTime = roundTime;
+        
+        timer = view.getTimer();
+        timer.setMaximum(this.roundTime);
+        
+        newRound();
+    }
+    
+    protected void startRound() {
+        Thread round = new Thread(this);
+        round.setDaemon(true);
+        t0 = System.currentTimeMillis();
+        round.start();
+    }
+    
+    protected void newRound() {
+        round++;
+        view.setRound(round);
+        timer.setValue(roundTime);
+    }
+    
+    private void setButtons(int buttonCount) {
+        JButton button;
+        for (int i = 0; i < buttonCount; i++) {
+            button = new JButton();
+            button.setActionCommand("B" + i);
+            
+            view.addButton(button);
+            buttons[i] = button;
+        }
+    }
+    
+    protected int getButtonCount() {
+        return buttons.length;
+    }
+
+    @Override
+    public void run() {
+        long t, dt;
+        
+        while(running) {
+            t = System.currentTimeMillis();
+            dt = t - t0;
+            
+            timer.setValue((int) (roundTime - dt));
+            
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) { }
+            
+            if(dt > roundTime)
+                running = false;
+        }
+    }
 }
